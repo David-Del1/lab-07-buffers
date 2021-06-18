a35cc3d419b02dc10db544d078b32b2d68ec278b
David Delgadillo <daviddel.731@gmail.com>
2021-06-18 09:08:26 -0700

    GET route done. test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,14 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a new order in our databse and sends a text message', async () => {
+  it.skip('creates a new order in our databse and sends a text message', async () => {
     const res = await request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 });
@@ -16,5 +17,13 @@ describe('demo routes', () => {
     expect(res.body).toEqual({ id: '1', quantity: 10 });
   });
 
-  
+  it('Selects and order by id via GET', async () => {
+    const order = await Order.insert({ quantity: 5 });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then(res => {
+        expect(res.body).toEqual(order);
+      });
+  });
+
 });
